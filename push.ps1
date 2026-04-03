e2adcdbe03bd28b9d5a0aa4a92b997386341f32e
NachiketKale <nachiket.kale24@spit.ac.in>
2026-04-03 20:32:34 +0530

    Initial commit âœ¨ Backend and Frontend scaffolding

---
+++ push.ps1
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit ✨ Backend and Frontend scaffolding"
+git branch -M main
+git remote add origin https://github.com/nachiketkale24-png/ai_environmental_monitoring.git
+git push -u origin main
